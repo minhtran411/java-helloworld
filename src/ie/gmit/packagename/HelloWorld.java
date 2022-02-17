f3948ebba4bc19ebc2e0895c6353bdfe873ef7ed
Daniel Cregg <danielcregg@users.noreply.github.com>
2022-02-17 11:34:18 +0000

    Clean up and adding commentary

--- src/ie/gmit/packagename/HelloWorld.java
+++ src/ie/gmit/packagename/HelloWorld.java
@@ -1,10 +1,9 @@
 package ie.gmit.packagename;
 
 class HelloWorld {
-
+    // Main method
     public static void main(String[] args) {
         // Print Hello World! to the console.
         System.out.println("Hello World!");
-    }
-
-}
+    } // End main method
+} // End class
