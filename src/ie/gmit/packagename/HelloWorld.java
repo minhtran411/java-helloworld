b95888cf8276cd53e7c9e24f9eb43912d429d11a
Daniel Cregg Gitpod <daniel.cregg@gmit.ie>
2022-02-17 11:52:54 +0000

    update

--- src/ie/gmit/packagename/HelloWorld.java
+++ src/ie/gmit/packagename/HelloWorld.java
@@ -4,6 +4,6 @@ class HelloWorld {
     // Main method
     public static void main(String[] args) {
         // Print Hello World! to the console.
-        System.out.println("Hello World! how are you!");
+        System.out.println("Hello World!");
     } // End main method
 } // End class
